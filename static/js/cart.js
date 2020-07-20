2e838e4a871c550f07e795b36982cbc5c19b9220
papanics <67746145+papanics@users.noreply.github.com>
2020-07-20 15:39:12 +0800

    send email

--- static/js/cart.js
+++ static/js/cart.js
@@ -5,5 +5,33 @@ for(var i = 0; i < updateBtns.length; i++){
         var productId = this.dataset.product
         var action = this.dataset.action
         console.log('productId:', productId, 'action:', action)
+
+        console.log('USER:', user)
+        if(user === 'AnonymousUser'){
+            console.log('Not logged in')
+        } else{
+            updateUserOrder(productId, action)
+        }
+    })
+}
+
+function updateUserOrder(productId, action){
+    console.log('User is logged in, sending data..')
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({'productId':productId, 'action':action})
+    })
+    .then((response)=>{
+        return response.json()
+    })
+
+    .then((data)=>{
+        console.log('data:', data)
     })
 }
